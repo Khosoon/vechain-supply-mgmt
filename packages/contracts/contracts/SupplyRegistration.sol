c36bd19e42a0d6ff71210a16bdc909f3e326b19a
Soon Kho <shkhoo1@gmail.com>
2024-12-02 12:47:48 +0530

    added the registration functionality for that

--- packages/contracts/contracts/SupplyRegistration.sol
+++ packages/contracts/contracts/SupplyRegistration.sol
@@ -1,1 +1,64 @@
-pragma
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SupplyRegistration {
+    struct Product {
+        string name;
+        uint256 registrationTime;
+        bool isRegistered;
+    }
+    
+    address public admin;
+    uint256 public currentStudentCount;
+    
+    mapping(address => Product) public products;
+    address[] public registeredProducts;
+    
+    event StudentRegistered(address indexed productAddress, string name, uint256 timestamp);
+    event RegistrationCancelled(address indexed productAddress);
+    
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can call this function");
+        _;
+    }
+
+    constructor(
+    ) {
+        admin = msg.sender;
+    }
+    
+    function registerForExam(string memory _name, address productAddress) external  {
+        require(!products[productAddress].isRegistered, "Already registered");
+        
+        products[productAddress] = Product({
+            name: _name,
+            registrationTime: block.timestamp,
+            isRegistered: true
+        });   
+        registeredProducts.push(productAddress);
+        currentStudentCount++;
+        
+    }
+    
+
+    function getRegisteredStudents() external onlyAdmin view returns (address[] memory) {
+        return registeredProducts;
+    }
+    
+    function getStudentDetails(address productAddress) 
+        external 
+        view 
+        returns (
+            string memory name,
+            uint256 registrationTime,
+            bool isRegistered
+        ) 
+    {
+        Product memory product = products[productAddress];
+        return (
+            product.name,
+            product.registrationTime,
+            product.isRegistered
+        );
+    }
+}
